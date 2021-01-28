3106d10dca46daac6317dceaadac7b2edfef6419
MehmetEmin94 <emin.aykan.ea@gmail.com>
2021-01-28 22:21:23 +0300

    28.01.2021

---
+++ Inheritance/Concrete/StarbucksCustomerManager.cs
@@ -0,0 +1,28 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceAbstractDemo.Concrete
+{
+    public class StarbucksCustomerManager : BaseCustomerManager
+    {
+        private IPersonCheckService _personCheckService;
+        public override void Save(Customer customer)
+        {
+            CheckIfRealPerson(customer);
+            base.Save(customer);
+        }
+
+        private void CheckIfRealPerson(Customer customer)
+        {
+            
+        }
+
+        void IPersonCheckService.CheckIfRealPerson(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorker[] workers = new IWorker[3]
+            {
+                new Manager(), new Worker(), new Robot()
+            };
+
+            foreach (var worker in workers)
+            {
+                worker.Work();
+            }
+            IEat[] eats = new IEat[2]
+            {
+                new Manager(), new Worker()
+            };
+            foreach (var eat in eats)
+            {
+                eat.Eat();
+            }
+            ISalary[] salaries = new ISalary[2]
+            {
+                new Manager(),new Worker()
+            };
+            foreach (var salary in salaries)
+            {
+                salary.GetSalary();
+            }
+        }
+    }
+    interface IWorker
+    {
+        void Work();
+        
+    }
+    interface IEat
+    {
+        void Eat();
+    }
+    interface ISalary
+    {
+        void GetSalary();
+    }
+    class Manager : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eatmanager");
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine("getsalarymanager");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("workmanager");
+        }
+    }
+
+    class Worker : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eatworker");
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine("getsalaryworker");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("workworker");
+        }
+    }
+
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            Console.WriteLine("workrobot");
+        }
+    }
+}

edf919a7bfad7a9a3282df8b5ee8b87dbc3b68dc
MehmetEmin94 <emin.aykan.ea@gmail.com>
2021-01-28 12:40:04 +0300

    28.01.2021

---
+++ OOP2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Individual customer1 = new Individual();
+            customer1.Id = 1;
+            customer1.CustomerNo = "12345";
+            customer1.Name = "ali";
+            customer1.Surname = "veli";
+            customer1.TcNo = "12345678910";
+
+            Corporate customer2 = new Corporate();
+            customer2.Id = 2;
+            customer2.CustomerNo = "98765";
+            customer2.CompanyName = "veliler";
+            customer2.TaxNo = "1234567890";
+
+            Customer customer3 = new Individual();
+            Customer customer4 = new Corporate();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+
+
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICreditManager personelFinanceCredit = new PersonelFinanceCredit();
+           
+            ICreditManager vehicleCreditManager = new VehicleCreditManager();
+            
+            ICreditManager housingCreditManager = new HousingCreditManager();
+
+            ILoggerService dataBaseLoggerService = new DataBaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            RecourseManager recourseManager = new RecourseManager();
+            recourseManager.DoRecourse(personelFinanceCredit, dataBaseLoggerService);
+
+            List<ICreditManager> credits = new List<ICreditManager>() { personelFinanceCredit, vehicleCreditManager };
+
+            //recourseManager.DoCreditPreinformation(credits);
+        }
+    }
+}
